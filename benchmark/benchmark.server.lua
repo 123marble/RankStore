afad7db491f400474d33b4717ce01203db04e706
123marble <63887804+123marble@users.noreply.github.com>
2024-08-18 23:11:57 +0100

    ✨ Finished implementations for SetScoreAsync, GetEntryAsync, GetTopScoresAsync. Example usage in benchmark.server.lua. 🎨 Added Util.Merge which is a generic optimised function for merging sorted lists. 🐛 leaderboardHelper.GetRank now checks if the leaderboard index exists so that this function does not error for empty leaderboards. 🎨 Added TimedCache:Clear instead of TimedCache:Cancel which can be used to force a cache refresh.

---
+++ benchmark/benchmark.server.lua
@@ -0,0 +1,17 @@
+local RankStore = require(game.ServerScriptService.RankStore)
+
+local rankStore = RankStore.GetRankStore("TestLeaderboard", 10)
+
+rankStore:ClearAsync()
+
+print(rankStore:GetTopScoresAsync(10))
+
+print(rankStore:SetScoreAsync(1, 25))
+print(rankStore:SetScoreAsync(2, 50))
+print(rankStore:SetScoreAsync(3, 20))
+print(rankStore:SetScoreAsync(4, 75))
+print(rankStore:SetScoreAsync(5, 45))
+
+print(rankStore:GetTopScoresAsync(10))
+
+print(rankStore:GetEntryAsync(3))
