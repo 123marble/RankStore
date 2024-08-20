2f539e2395958ed1b23919e35d4243bbbe92ce8a
123marble <63887804+123marble@users.noreply.github.com>
2024-08-20 21:50:03 +0100

    🎨 Added BucketsStore:_GetSummedRanksOverBuckets which is enabled code reuse within BucketsStore:SetScoreAsync and BucketsStore:FindRank.

--- benchmark/benchmark.server.lua
+++ benchmark/benchmark.server.lua
@@ -1,6 +1,6 @@
 local RankStore = require(game.ServerScriptService.RankStore)
 
-local rankStore = RankStore.GetRankStore("TestLeaderboard", 10)
+local rankStore = RankStore.GetRankStore("TestLeaderboard_1", 4)
 
 rankStore:ClearAsync()
 
@@ -11,6 +11,7 @@ print(rankStore:SetScoreAsync(2, 50))
 print(rankStore:SetScoreAsync(3, 20))
 print(rankStore:SetScoreAsync(4, 75))
 print(rankStore:SetScoreAsync(5, 45))
+print(rankStore:SetScoreAsync(1, 65))
 
 print(rankStore:GetTopScoresAsync(10))
 
